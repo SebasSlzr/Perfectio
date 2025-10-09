1d8bd3e1d4b922116ead8eb4dd50e19be9429614
Andres54 <andrescastrillon2005@gmail.com>
2025-10-09 00:02:16 -0500

    Main con nombre del proyecto

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from typing import Union
-
+from app.routes import goal_routes
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Perfectio")
+
+app.include_router(goal_routes.router, prefix="/goals", tags=["Goals"])
 
 
-@app.get("/")
+@app.get("/", tags=["Root"])
 def read_root():
-    return {"Hello": "Amogus"}
+    return {
+        "message": "Bienvenido a Perfectio",
+        "version": "1.0.0"
+    }
