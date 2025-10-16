8d9d2fe90e8aed322180f0d0c1d423ce709b8b58
Sebastian Salazar <sebas.salazar.ruano28@gmail.com>
2025-10-15 21:03:36 -0500

    Se solucionaron conflictos entre feature/reminders y Dev, ademas que se añadio propiamente reminders al main

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI(
 
 app.include_router(goal_routes.router, prefix="/goals", tags=["Goals"])
 app.include_router(habit_routes.router, prefix="/habits", tags=["Habits"])
-app.include_router(reminder_routes.router)
+app.include_router(reminder_routes.router, prefix="/reminders", tags=["Reminders"])
 @app.get("/", tags=["Root"])
 def read_root():
     return {
