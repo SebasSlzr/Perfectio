599245996349c5be4a397fe27f1cf29502eb136a
Sebastian Salazar <sebas.salazar.ruano28@gmail.com>
2025-10-28 16:51:00 -0500

    se implemento la autenticaicon al main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes import goal_routes
 from app.routes import habit_routes
 from app.routes import reminder_routes
+from app.routes import auth_routes
 
 app = FastAPI(
     title="Perfectio"
@@ -10,6 +11,7 @@ app = FastAPI(
 app.include_router(goal_routes.router, prefix="/goals", tags=["Goals"])
 app.include_router(habit_routes.router, prefix="/habits", tags=["Habits"])
 app.include_router(reminder_routes.router, prefix="/reminders", tags=["Reminders"])
+app.include_router(auth_routes.router, prefix="/auth", tags=["Authentication"])
 @app.get("/", tags=["Root"])
 def read_root():
     return {
