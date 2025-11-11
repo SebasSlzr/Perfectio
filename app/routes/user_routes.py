f8d00554852d1e9e95fe44389e9fddb8e53c52ec
Sebastian Salazar <sebas.salazar.ruano28@gmail.com>
2025-11-11 14:48:21 -0500

    Se agrego el controller de usuarios y se modificaron las rutas de usuarios para poder hacer las peticiones desde el swagger, quedaron listas 5 peticiones HTTP para users

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -3,25 +3,26 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_db
 from app.schemas import User, UserCreate, UserUpdate
+from app.controllers import user_controller as controller
 
-router = APIRouter()
+router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.get("/", response_model=list[User])
 async def list_users(db: AsyncSession = Depends(get_db)):
-    return {"msg": "Lista de todos los usuarios"}
+    return await controller.get_all_users(db)
 
 @router.get("/{user_id}", response_model=User)
 async def get_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    return {"msg": f"Información del usuario {user_id}"}
+    return await controller.get_user_by_id(user_id, db)
 
 @router.post("/", response_model=User)
 async def create_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
-    return {"msg": "Usuario creado correctamente"}
+    return await controller.create_user(user, db)
 
 @router.put("/{user_id}", response_model=User)
 async def update_user(user_id: int, user: UserUpdate, db: AsyncSession = Depends(get_db)):
-    return {"msg": f"Usuario {user_id} actualizado"}
+    return await controller.update_user(user_id, user, db)
 
 @router.delete("/{user_id}")
 async def delete_user(user_id: int, db: AsyncSession = Depends(get_db)):
-    return {"msg": f"Usuario {user_id} eliminado"}
+    return await controller.delete_user(user_id, db)
